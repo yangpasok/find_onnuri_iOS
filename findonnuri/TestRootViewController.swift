f24bdfbed7f3a92aefe7e07c4492ee514c3707bf
sangwu.wi <sangwu.wi@navercorp.com>
2020-04-08 18:38:03 +0900

    초기 프로젝트 생성

---
+++ findonnuri/TestRootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  findonnuri
+//
+//  Created by sangwu on 2020/04/08.
+//  Copyright © 2020 YEPPI. All rights reserved.
+//
+
+import UIKit
+
+class TestRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+}
+
